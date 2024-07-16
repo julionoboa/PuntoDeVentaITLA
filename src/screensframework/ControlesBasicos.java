8ed4c0cc7c19212f0f14af765be8779791f35bf9
Julio Noboa <julionoboa26@gmail.com>
2024-07-15 22:35:04 -0400

    initial commit

---
+++ src/screensframework/ControlesBasicos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package screensframework;
+
+import javafx.application.Platform;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Wil
+ */
+public class ControlesBasicos {
+    
+    public void salirSistema() {
+        int pregunta = JOptionPane.showConfirmDialog(null, "Realmente desea salir del programa?");
+
+        //Se cambio el yes por JOptionPane.YES_OPTION
+        if (pregunta == JOptionPane.YES_OPTION) {
+            Platform.exit();
+        } 
+    }
+}
